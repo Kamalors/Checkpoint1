9908910480e1821a2b9bc1fa709141d4316ddb24
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-03-29 14:18:15 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -10,7 +10,7 @@ for nom in "$@"; do
     if cat /etc/passwd | grep "^$nom:" > /dev/null; then
         echo "L'utilisateur $nom existe déjà."
     else
-        if useradd --force-badname "$nom"; then
+        if useradd "$nom"; then
             echo "L'utilisateur $nom a été créé."
         else
             echo "Erreur à la création de l'utilisateur $nom."
